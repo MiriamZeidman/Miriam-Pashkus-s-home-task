8d109da37ade75261bb5b83d4e0a2439df61432b
Miriam Zeidman <zeidman@g.jct.ac.il>
2024-03-20 12:36:14 +0200

    define API get by user ID

--- ex1/routes/users.js
+++ ex1/routes/users.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', function(req, res, next) {
+  var userId = req.params.userId; // Get the user ID from the request parameters
+  var userDetails=
+    {
+      "name": "Miriam Pashkus",
+      "user id":userId,
+      "adress": "Kipodan 5, Jerusalem",
+      "date of birth": "17/02/2002",
+      "phone": "02-5800871",
+      "mobile phone": "053-4678502"
+    }
+  res.send(userDetails);
 });
 
+router.delete('/:userId', function(req, res, next) {
+  var userId = req.params.userId; // Get the user ID from the request parameters
+  // Do whatever you need to do with the user ID
+  res.send(`The user ${userId} deleted from the list`);
+});
 module.exports = router;
