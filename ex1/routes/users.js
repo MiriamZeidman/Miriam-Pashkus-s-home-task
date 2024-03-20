862ef1a4211da00017831959254d1a66f3c14ff0
Miriam Zeidman <zeidman@g.jct.ac.il>
2024-03-20 13:21:02 +0200

    define API add by user id

--- ex1/routes/users.js
+++ ex1/routes/users.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+router.post('/:id', function(req, res, next) {
+    var userId = req.body.id; // Get the user ID from the request parameters
+    // Do whatever you need to do with the user ID
+    res.send(`The user ${userId} added to the list`);
+});
 /* GET users listing. */
 router.get('/:userId', function(req, res, next) {
   var userId = req.params.userId; // Get the user ID from the request parameters
   var userDetails=
     {
       "name": "Miriam Pashkus",
-      "user id":userId,
+      "user id":"userId",
       "adress": "Kipodan 5, Jerusalem",
       "date of birth": "17/02/2002",
       "phone": "02-5800871",
