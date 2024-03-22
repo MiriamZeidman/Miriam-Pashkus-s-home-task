8d980f69058a78c8bfe443ba34ae037e23fe871d
Miriam Zeidman <zeidman@g.jct.ac.il>
2024-03-22 11:25:57 +0200

    write the post method that add user to the data base

--- ex1/routes/users.js
+++ ex1/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const Model = require('../model/model');
 router.post('/:id', function(req, res, next) {
     var userId = req.body.id; // Get the user ID from the request parameters
-    // Do whatever you need to do with the user ID
-    res.send(`The user ${userId} added to the list`);
+    const data = new Model({
+        name:req.body.name,
+        id:req.body.id,
+        address:req.body.address,
+
+    })
+    try {
+        const dataToSave = data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }    // Do whatever you need to do with the user ID
 });
 /* GET users listing. */
 router.get('/:userId', function(req, res, next) {
